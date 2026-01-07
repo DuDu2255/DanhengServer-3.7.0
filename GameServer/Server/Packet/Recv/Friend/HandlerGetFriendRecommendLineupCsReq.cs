dd5571d3c7a9d59ced66d87d88a4d930b9d978ff
zb1240610539-del <zb1240610539@gmail.com>
2026-01-07 16:55:47 +0800

    Update HandlerGetFriendRecommendLineupCsReq.cs

--- GameServer/Server/Packet/Recv/Friend/HandlerGetFriendRecommendLineupCsReq.cs
+++ GameServer/Server/Packet/Recv/Friend/HandlerGetFriendRecommendLineupCsReq.cs
@@ -8,15 +8,15 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Friend;
 [Opcode(CmdIds.GetFriendRecommendLineupCsReq)]
 public class HandlerGetFriendRecommendLineupCsReq : Handler
 {
-    public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
-    {
-        // 解析请求，获取 ChallengeId (对应协议里的 Key)
-        var req = GetFriendRecommendLineupCsReq.Parser.ParseFrom(data);
-        
-        // 生成全服战报列表
-        var rspData = connection.Player!.FriendManager!.GetGlobalRecommendLineup(req.Key);
-        
-        // 发送回包给客户端
-        await connection.SendPacket(new PacketGetFriendRecommendLineupScRsp(rspData));
-    }
+   // 在 HandlerGetFriendRecommendLineupCsReq.cs 中
+public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
+{
+    // 直接用响应包的 Parser 解析！
+    var req = GetFriendRecommendLineupScRsp.Parser.ParseFrom(data);
+    
+    // 只要这一步能拿到 Key (也就是 ChallengeId)，后面就全通了
+    var rspData = connection.Player!.FriendManager!.GetGlobalRecommendLineup(req.Key);
+    
+    await connection.SendPacket(new PacketGetFriendRecommendLineupScRsp(rspData));
+}
 }
