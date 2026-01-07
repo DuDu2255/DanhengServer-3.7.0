1a1440a04ed6b6e6a040a328f11d65a765960425
zb1240610539-del <zb1240610539@gmail.com>
2026-01-07 15:48:01 +0800

    Enhance comments in HandlerGetFriendRecommendLineupCsReq
    Added comments to clarify request parsing and response generation.

--- GameServer/Server/Packet/Recv/Friend/HandlerGetFriendRecommendLineupCsReq.cs
+++ GameServer/Server/Packet/Recv/Friend/HandlerGetFriendRecommendLineupCsReq.cs
@@ -1,13 +1,22 @@
+
+using EggLink.DanhengServer.Kcp;
+using EggLink.DanhengServer.Proto;
+using EggLink.DanhengServer.GameServer.Server.Packet.Send.Friend;
+
+namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Friend;
+
 [Opcode(CmdIds.GetFriendRecommendLineupCsReq)]
 public class HandlerGetFriendRecommendLineupCsReq : Handler
 {
     public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
     {
+        // 解析请求，获取 ChallengeId (对应协议里的 Key)
         var req = GetFriendRecommendLineupCsReq.Parser.ParseFrom(data);
         
-        // 调用逻辑获取全服阵容
+        // 生成全服战报列表
         var rspData = connection.Player!.FriendManager!.GetGlobalRecommendLineup(req.Key);
         
+        // 发送回包给客户端
         await connection.SendPacket(new PacketGetFriendRecommendLineupScRsp(rspData));
     }
 }
