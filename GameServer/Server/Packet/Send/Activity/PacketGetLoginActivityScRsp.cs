cd068a39231174b7dd06074680f5d64e7ebce9b9
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 11:06:51 +0800

    签到
    Updated constructor to accept PlayerInstance and improved comments.

--- GameServer/Server/Packet/Send/Activity/PacketGetLoginActivityScRsp.cs
+++ GameServer/Server/Packet/Send/Activity/PacketGetLoginActivityScRsp.cs
@@ -1,15 +1,25 @@
+using EggLink.DanhengServer.Kcp;
+using EggLink.DanhengServer.Proto;
+using Google.Protobuf;
+
+namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
+
 public class PacketGetLoginActivityScRsp : NetPacket
 {
-    public PacketGetLoginActivityScRsp(Player player) : base(CmdIds.GetLoginActivityScRsp)
+    // 构造函数，传入 Player 对象以获取其活动进度
+    public PacketGetLoginActivityScRsp(PlayerInstance player) : base(CmdIds.GetLoginActivityScRsp)
     {
         var rsp = new GetLoginActivityScRsp
         {
             Retcode = 0
         };
 
-        // 从 ActivityData 转换为 Proto 列表 (使用你之前写的 ToProto 方法)
-        rsp.LoginActivityList.AddRange(player.ActivityManager.Data.LoginActivityData.ToProto());
+        // 调用你之前在 ActivityData.cs 中定义的 ToProto 方法
+        // 这一步会将数据库中的登录天数和已领奖励列表填充进响应包
+        var loginDataList = player.ActivityManager.Data.LoginActivityData.ToProto();
+        rsp.LoginActivityList.AddRange(loginDataList);
 
+        // 将 Protobuf 对象序列化为字节数组存入 NetPacket 的 Data
         this.Data = rsp.ToByteArray();
     }
 }
