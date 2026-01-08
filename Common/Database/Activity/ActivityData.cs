1e1409653aecb1d7c38fd9f6e9cc5b8ff6869d36
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 11:25:54 +0800

    Add LoginActivityData class to ActivityData
    Added LoginActivityData class to manage login activity data.

--- Common/Database/Activity/ActivityData.cs
+++ Common/Database/Activity/ActivityData.cs
@@ -7,8 +7,28 @@ namespace EggLink.DanhengServer.Database.Activity;
 public class ActivityData : BaseDatabaseDataHelper
 {
     [SugarColumn(IsJson = true)] public TrialActivityData TrialActivityData { get; set; } = new();
+    // 必须添加这一行，否则 PacketGetLoginActivityScRsp 会报错
+    [SugarColumn(IsJson = true)] public LoginActivityData LoginActivityData { get; set; } = new();
 }
+public class LoginActivityData
+{
+    public Dictionary<uint, List<uint>> TakenRewards { get; set; } = new();
+    public Dictionary<uint, uint> LoginDays { get; set; } = new();
+    public long LastUpdateTick { get; set; }
 
+    public List<Proto.LoginActivityData> ToProto()
+    {
+        var proto = new List<Proto.LoginActivityData>();
+        foreach (var kv in LoginDays)
+        {
+            var data = new Proto.LoginActivityData { Id = kv.Key, LoginDays = kv.Value };
+            if (TakenRewards.TryGetValue(kv.Key, out var takenList))
+                data.MLGBIGIECCO.AddRange(takenList);
+            proto.Add(data);
+        }
+        return proto;
+    }
+}
 public class TrialActivityData
 {
     public List<TrialActivityResultData> Activities { get; set; } = new();
