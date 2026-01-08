90a124248ecdac9bc52eb2adc05ac566504af881
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 14:35:01 +0800

    Update PacketTakeLoginActivityRewardScRsp.cs

--- GameServer/Server/Packet/Send/Activity/PacketTakeLoginActivityRewardScRsp.cs
+++ GameServer/Server/Packet/Send/Activity/PacketTakeLoginActivityRewardScRsp.cs
@@ -5,20 +5,17 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
 
 public class PacketTakeLoginActivityRewardScRsp : BasePacket
 {
-    // 参数修正：将 List<ItemData> 改为 ItemList
-    public PacketTakeLoginActivityRewardScRsp(uint activityId, uint takeDays, uint retcode, ItemList rewards) 
-        : base((ushort)CmdIds.TakeLoginActivityRewardScRsp) 
+    public PacketTakeLoginActivityRewardScRsp(uint activityId, uint takeDays, uint retcode, ItemList rewards, uint panelId) 
+    : base((ushort)CmdIds.TakeLoginActivityRewardScRsp) 
+  {
+    var proto = new TakeLoginActivityRewardScRsp
     {
-        var proto = new TakeLoginActivityRewardScRsp
-        {
-            Id = activityId,
-            TakeDays = takeDays,
-            Retcode = retcode,
-            Reward = rewards, // 这里直接赋值，不需要再 Select 转换
-            PanelId = 10130 
-        };
-
-        // 根据 3.7.0 的 BasePacket 定义使用 SetData
-        SetData(proto); 
-    }
+        Id = activityId,
+        TakeDays = takeDays,
+        Retcode = retcode,
+        Reward = rewards,
+        PanelId = panelId // 使用动态传入的值
+    };
+    SetData(proto); 
+  }
 }
