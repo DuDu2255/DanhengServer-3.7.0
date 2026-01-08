657489ade4f327b60396810d530b0dc69013755b
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 23:33:20 +0800

    Update HandlerTakeLoginActivityRewardCsReq.cs

--- GameServer/Server/Packet/Recv/Activity/HandlerTakeLoginActivityRewardCsReq.cs
+++ GameServer/Server/Packet/Recv/Activity/HandlerTakeLoginActivityRewardCsReq.cs
@@ -1,6 +1,7 @@
+// 文件路径: GameServer/Server/Packet/Recv/Activity/HandlerTakeLoginActivityRewardCsReq.cs
 using EggLink.DanhengServer.GameServer.Server;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
-using EggLink.DanhengServer.Kcp; // Opcode 所在的命名空间
+using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Activity;
@@ -13,13 +14,12 @@ public class HandlerTakeLoginActivityRewardCsReq : Handler
         var req = TakeLoginActivityRewardCsReq.Parser.ParseFrom(data);
         var player = connection.Player;
 
-        // 判空保护，消除警告
         if (player?.ActivityManager == null) return;
 
-        // 解包异步元组 (items, panelId, retcode)
+        // 解包 ActivityManager 返回的 (items, panelId, retcode)
         var (rewardProto, panelId, retcode) = await player.ActivityManager.TakeLoginReward(req.Id, req.TakeDays);
 
-        // 发送回包
+        // 发送 Packet，带入动态 panelId
         await connection.SendPacket(new PacketTakeLoginActivityRewardScRsp(req.Id, req.TakeDays, retcode, rewardProto, panelId));
     }
 }
