fa372e77afdb9a574f6704b45cc78d5cb65827ce
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 14:54:02 +0800

    Update HandlerTakeLoginActivityRewardCsReq.cs

--- GameServer/Server/Packet/Recv/Activity/HandlerTakeLoginActivityRewardCsReq.cs
+++ GameServer/Server/Packet/Recv/Activity/HandlerTakeLoginActivityRewardCsReq.cs
@@ -1,25 +1,25 @@
 using EggLink.DanhengServer.GameServer.Server;
-using EggLink.DanhengServer.GameServer.Server.Packet; // 必须添加这个引用
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
+using EggLink.DanhengServer.Kcp; // Opcode 所在的命名空间
 using EggLink.DanhengServer.Proto;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Recv.Activity;
 
-internal class HandlerTakeLoginActivityRewardCsReq : BasePacketHandler
+[Opcode(CmdIds.TakeLoginActivityRewardCsReq)]
+public class HandlerTakeLoginActivityRewardCsReq : Handler
 {
-    public override uint CmdId => CmdIds.TakeLoginActivityRewardCsReq;
-
     public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
     {
         var req = TakeLoginActivityRewardCsReq.Parser.ParseFrom(data);
         var player = connection.Player;
 
+        // 判空保护，消除警告
         if (player?.ActivityManager == null) return;
 
-        // 注意这里的解包顺序：items, panelId, retcode
+        // 解包异步元组 (items, panelId, retcode)
         var (rewardProto, panelId, retcode) = await player.ActivityManager.TakeLoginReward(req.Id, req.TakeDays);
 
-        // 发送 Packet
+        // 发送回包
         await connection.SendPacket(new PacketTakeLoginActivityRewardScRsp(req.Id, req.TakeDays, retcode, rewardProto, panelId));
     }
 }
