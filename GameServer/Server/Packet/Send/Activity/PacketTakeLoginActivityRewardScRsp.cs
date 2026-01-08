00408d5023ed61c7c76ce946ff65501a91c78d43
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 11:21:43 +0800

    领取奖励

--- GameServer/Server/Packet/Send/Activity/PacketTakeLoginActivityRewardScRsp.cs
+++ GameServer/Server/Packet/Send/Activity/PacketTakeLoginActivityRewardScRsp.cs
@@ -1,3 +1,4 @@
+using EggLink.DanhengServer.Database.Inventory;
 using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
 
@@ -5,18 +6,26 @@ namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
 
 public class PacketTakeLoginActivityRewardScRsp : BasePacket
 {
-    public PacketTakeLoginActivityRewardScRsp(TakeLoginActivityRewardCsReq req, uint retcode, ItemList reward) 
-        : base((ushort)CmdIds.TakeLoginActivityRewardScRsp)
+    // 参数：活动ID，领取的天数，返回码，奖励列表
+    public PacketTakeLoginActivityRewardScRsp(uint activityId, uint takeDays, uint retcode, List<ItemData> rewards) 
+        : base(CmdIds.TakeLoginActivityRewardScRsp) // 必须使用签到奖励的ID
     {
-        var rsp = new TakeLoginActivityRewardScRsp
+        var proto = new TakeLoginActivityRewardScRsp
         {
+            Id = activityId,
+            TakeDays = takeDays,
             Retcode = retcode,
-            Id = req.Id,
-            TakeDays = req.TakeDays,
-            Reward = reward,
-            PanelId = 10130 // 默认面板ID
+            Reward = new ItemList(),
+            PanelId = 10130 // 默认签到面板ID，也可以动态传入
         };
 
-        this.Data = rsp.ToByteArray();
+        // 填充奖励物品
+        if (rewards != null && rewards.Count > 0)
+        {
+            proto.Reward.ItemList_.Add(rewards.Select(x => x.ToProto()).ToArray());
+        }
+
+        // 3.7.0版本通常使用 SetData 或 this.Data = ...
+        SetData(proto); 
     }
 }
