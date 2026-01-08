9fb5502c89a7f06f8c4706254b997391971dcc19
zb1240610539-del <zb1240610539@gmail.com>
2026-01-08 23:36:50 +0800

    Update PacketGetLoginActivityScRsp.cs

--- GameServer/Server/Packet/Send/Activity/PacketGetLoginActivityScRsp.cs
+++ GameServer/Server/Packet/Send/Activity/PacketGetLoginActivityScRsp.cs
@@ -1,27 +1,14 @@
 using EggLink.DanhengServer.Kcp;
 using EggLink.DanhengServer.Proto;
-using EggLink.DanhengServer.GameServer.Game.Player;
-using Google.Protobuf;
 
 namespace EggLink.DanhengServer.GameServer.Server.Packet.Send.Activity;
 
-// 在 3.7.0 版本中，基类是 BasePacket
 public class PacketGetLoginActivityScRsp : BasePacket
 {
-    public PacketGetLoginActivityScRsp(PlayerInstance player) : base(CmdIds.GetLoginActivityScRsp)
+    // 构造函数，传入 Proto 数据，并指定对应的 CmdId
+    public PacketGetLoginActivityScRsp(GetLoginActivityScRsp proto) 
+        : base((ushort)CmdIds.GetLoginActivityScRsp) 
     {
-        var rsp = new GetLoginActivityScRsp
-        {
-            Retcode = 0
-        };
-
-       // 确保 ActivityManager 不为空再调用
-      if (player.ActivityManager != null) {
-       var loginDataList = player.ActivityManager.Data.LoginActivityData.ToProto();
-       rsp.LoginActivityList.AddRange(loginDataList);
-		}
-
-        // 设置 BasePacket 的 Data 属性
-        this.Data = rsp.ToByteArray();
+        this.SetData(proto);
     }
 }
