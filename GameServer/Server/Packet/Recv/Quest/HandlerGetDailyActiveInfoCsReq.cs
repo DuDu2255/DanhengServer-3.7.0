0d7708885e88bd169bf436abfc18455feb9f125e
zb1240610539-del <zb1240610539@gmail.com>
2026-01-09 16:08:12 +0800

    Update HandlerGetDailyActiveInfoCsReq.cs

--- GameServer/Server/Packet/Recv/Quest/HandlerGetDailyActiveInfoCsReq.cs
+++ GameServer/Server/Packet/Recv/Quest/HandlerGetDailyActiveInfoCsReq.cs
@@ -10,7 +10,7 @@ public class HandlerGetDailyActiveInfoCsReq : Handler
     public override async Task OnHandle(Connection connection, byte[] header, byte[] data)
     {
         // 1. 准备硬编码数据 (调用 QuestManager 里的函数)
-        var rsp = connection.Player!.QuestManager!.GetDailyActiveInfo();
+        var rsp = connection.Player!.DailyActiveManager!.GetDailyActiveInfo();
         
         // 2. 构造你刚写的发送包
         var packet = new PacketGetDailyActiveInfoScRsp(rsp);
@@ -19,6 +19,5 @@ public class HandlerGetDailyActiveInfoCsReq : Handler
         await connection.SendPacket(packet);
         
         // 4. 为了让界面刷出来，强行推一下任务状态 (QuestStatus.Doing)
-        await connection.Player.QuestManager.ForceSyncDailyQuests();
-    }
+        await connection.Player.DailyActiveManager.SyncDailyQuestsStatus();    }
 }
