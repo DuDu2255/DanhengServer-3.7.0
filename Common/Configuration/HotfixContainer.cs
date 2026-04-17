bf9677358230c04d5c189de8bc1231545bad4e13
mikugirls <mikugirls@163.com>
2026-04-18 00:27:40 +0800

    Update HotfixContainer.cs

--- Common/Configuration/HotfixContainer.cs
+++ Common/Configuration/HotfixContainer.cs
@@ -14,11 +14,11 @@ public class HotfixContainer
 
 public class DownloadUrlConfig
 {
-    public string AssetBundleUrl { get; set; } = "";
+    public string AssetBundleUrl { get; set; } = "asb/V3.7Live/output_12477160_5da2dc8d6e33_785951683a8623";
     public string ExAssetBundleUrl { get; set; } = "";
-    public string ExResourceUrl { get; set; } = "";
-    public string LuaUrl { get; set; } = "";
-    public string IfixUrl { get; set; } = "";
+    public string ExResourceUrl { get; set; } = "design_data/V3.7Live/output_12611473_1ea87ad3f42a_748fabe9f3533c";
+    public string LuaUrl { get; set; } = "lua/V3.7Live/output_12576450_666f169b3262_4d113c227ffbe2";
+    public string IfixUrl { get; set; } = "ifix/V3.7Live/output_12623363_6a3715f341cc_9169fbfe61efd7";
 }
 
 public static class GateWayBaseUrl
@@ -32,5 +32,5 @@ public static class GateWayBaseUrl
 public static class BaseUrl
 {
     public const string CN = "https://autopatchcn.bhsr.com/";
-    public const string OS = "https://autopatchos.starrails.com/";
+    public const string OS = "https://autopatchcn.bhsr.com/";
 }
